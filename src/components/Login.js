9a566fb396d5c188fb24176dcd233a76d906e95c
= <nncl@live.com>
2018-10-09 23:27:22 -0300

    Requesting auth route

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,36 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 export default class Login extends Component {
+
+    constructor() {
+        super();
+        this.state = {msg: null};
+        this.send = this.send.bind(this);
+    }
+
+    send(e) {
+        e.preventDefault();
+        this.setState({msg: null});
+
+        const data = {
+            login: this.nickname.value
+            , senha: this.password.value
+        };
+
+        axios.post('https://instalura-api.herokuapp.com/api/public/login', data)
+            .then((res) => console.log(res))
+            .catch((err) => this.setState({msg: err.message}));
+    }
+
     render() {
         return (
             <div className="login-box">
                 <h1 className="header-logo">Instalura</h1>
-                <form>
-                    <input type="text"/>
-                    <input type="password"/>
+                <span>{this.state.msg}</span>
+                <form onSubmit={this.send}>
+                    <input type="text" ref={(input) => this.nickname = input}/>
+                    <input type="password" ref={(input) => this.password = input}/>
                     <input type="submit" value="login"/>
                 </form>
             </div>
