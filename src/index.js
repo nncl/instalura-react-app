eacc90cd1d80f74b7f7ca2472b384fe6400ce6f8
= <nncl@live.com>
2018-10-10 00:23:16 -0300

    Creating private route component

--- src/index.js
+++ src/index.js
@@ -3,13 +3,35 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Login from './components/Login';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
+
+const checkToken = () => {
+    return localStorage.getItem('token')
+};
+
+const PrivateRoute = ({component: Component, ...rest}) => (
+    <Route
+        {...rest}
+        render={props =>
+            checkToken() ? (
+                <Component {...props} />
+            ) : (
+                <Redirect
+                    to={{
+                        pathname: "/",
+                        state: {from: props.location}
+                    }}
+                />
+            )
+        }
+    />
+);
 
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
             <Route path="/" exact={true} component={Login}/>
-            <Route path="/timeline" component={App}/>
+            <PrivateRoute path="/timeline" component={App}/>
         </Switch>
     </ BrowserRouter>
     , document.getElementById('root'));
