3fae94842f65aaa4d12d9f8948132daf0b3bb3f8
= <nncl@live.com>
2018-10-09 23:06:08 -0300

    Creating login route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from './components/Login';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact={true} component={Login}/>
+            <Route path="/timeline" component={App}/>
+        </Switch>
+    </ BrowserRouter>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
